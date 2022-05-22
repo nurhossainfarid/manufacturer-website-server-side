a8e687f2a12d0f793a5aee8e54d34ee2a49d18c1
nurhossainfarid <faahsan1516@gmail.com>
2022-05-22 19:59:58 +0600

    add heroku with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
@@ -7,6 +8,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+// Mongodb server
+const uri = `mongodb+srv://${process.env.ACCESS_USER}:${process.env.ACCESS_PASSWORD}@cluster0.nd7ni.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async() => {
+    try {
+        await client.connect();
+        const carCollection = client.db('carCollection').collection('products');
+
+        /* -------------------------------------- car Collection --------------------------------------------- */
+        app.get('/products', async (req, res) => {
+            const result = await carCollection.find().toArray();
+            res.send(result);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir);
+
 
 
 // Initial check
