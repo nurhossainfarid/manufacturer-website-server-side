296a4e1178dc563d21a645bbbf9b007ef2701411
nurhossainfarid <faahsan1516@gmail.com>
2022-05-25 20:07:41 +0600

    add api for admin

--- index.js
+++ index.js
@@ -41,6 +41,17 @@ const run = async() => {
         const commentCollection = client.db('carCollection').collection('comments');
         const profileCollection = client.db('profileCollection').collection('profiles');
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+              next();
+            }
+            else {
+              res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
 
         /* -------------------------------------- car Collection --------------------------------------------- */
         app.get('/products', async (req, res) => {
@@ -66,6 +77,24 @@ const run = async() => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isRole = user.role == 'admin';
+            res.send({admin : isRole});
+        })
+        
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+              const filter = { email: email };
+              const updateDoc = {
+                $set: { role: 'admin' },
+              };
+              const result = await userCollection.updateOne(filter, updateDoc);
+              res.send(result);
+        })
+        
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
